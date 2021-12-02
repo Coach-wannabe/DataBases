e6aba764d43424e76ee8d3a371b6c620ae7a81c4
Coach-wannabe667 <85020618+Coach-wannabe667@users.noreply.github.com>
2021-12-02 15:31:12 +0600

    Update task2.sql

--- lab8/task2.sql
+++ lab8/task2.sql
@@ -1,47 +1,73 @@
--- Task 1
+-- Task 2
 -- a
-create or replace function increment(value integer)
-returns integer
-language plpgsql as
+create table t1(
+    id integer,
+    first_name varchar(40) not null,
+    last_name varchar(40)
+);
+
+create table t2(
+    name varchar(40) not null,
+    time timestamp
+);
+
+create or replace function the_time()
+    returns trigger
+    language plpgsql as
 $$
-begin
-    return value + 1;
-end;
+    begin
+        insert into t2(name, time) values (new.first_name, now());
+
+        return new;
+    end;
 $$;
+
+create trigger return_timestamp
+    after insert
+    on t1
+    for each row
+execute procedure the_time();
+
+insert into t1 (id, first_name, last_name)
+VALUES (1, 'John', 'Brown');
+insert into t1 (id, first_name, last_name)
+values (2, 'Adilet', 'Bolarbekov');
+
+select * from t1;
+select * from t2;
 
 -- b
-create or replace function sum(a integer, b integer)
-returns integer
-language plpgsql as
-$$
-begin
-    return a + b;
-end;
-$$;
-select sum(10, 15);
+create table t3(
+    first_name varchar(20),
+    last_name varchar(20),
+    date_of_birth date
+);
 
--- c
-create or replace function check_function(value integer)
-returns boolean
-language plpgsql as
+create table t4(
+    age varchar(100)
+);
+
+create or replace function the_age()
+    returns trigger
+    language plpgsql as
 $$
-begin
-    if value % 2 = 0 then
-        return true;
-    else return false;
-    end if;
-end;
+    begin
+        insert into t4(age)
+        values (age(now(), new.date_of_birth));
+
+        return new;
+    end;
 $$;
 
--- d
-create or replace function check_password(t text)
-returns boolean
-language plpgsql as
-$$
-begin
-    if length(t) > 12 then
-        return true;
-    else return false;
-    end if;
-end;
-$$;
+create trigger return_age
+    after insert
+    on t3
+    for each row
+execute procedure the_age();
+
+insert into t3(first_name, last_name, date_of_birth)
+VALUES ('Adilet', 'Bolatbekov', '2002-12-08'),
+       ('Steven', 'Stevenson', '1963-12-21');
+
+select * from t3;
+select * from t4;

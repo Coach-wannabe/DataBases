22a030f0669ac852ce60a87cf3c2d1b09d8ed2a2
Coach-wannabe667 <85020618+Coach-wannabe667@users.noreply.github.com>
2021-12-02 15:29:19 +0600

    Create task1.sql

---
+++ lab8/task1.sql
@@ -0,0 +1,59 @@
+-- Task 1
+-- a
+create or replace function increment(value integer)
+returns integer
+language plpgsql as
+$$
+begin
+    return value + 1;
+end;
+$$;
+
+-- b
+create or replace function sum(a integer, b integer)
+returns integer
+language plpgsql as
+$$
+begin
+    return a + b;
+end;
+$$;
+select sum(10, 15);
+
+-- c
+create or replace function check_function(value integer)
+returns boolean
+language plpgsql as
+$$
+begin
+    if value % 2 = 0 then
+        return true;
+    else return false;
+    end if;
+end;
+$$;
+
+-- d
+create or replace function check_password(t text)
+returns boolean
+language plpgsql as
+$$
+begin
+    if length(t) > 12 then
+        return true;
+    else return false;
+    end if;
+end;
+$$;
+
+-- e
+create or replace function two_outputs(in value int, out n1 int, out n2 int)
+language plpgsql as
+$$
+begin
+    n1 = value + 1;
+    n2 = value - 1;
+end;
+$$;
+
+select two_outputs(10);

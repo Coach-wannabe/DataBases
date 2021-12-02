80d88e90c24eeff28a4a789fab1d56483191ec94
Coach-wannabe667 <85020618+Coach-wannabe667@users.noreply.github.com>
2021-12-03 00:03:05 +0600

    Update task2.sql

--- lab8/task2.sql
+++ lab8/task2.sql
@@ -71,3 +71,74 @@ VALUES ('Adilet', 'Bolatbekov', '2002-12-08'),
 
 select * from t3;
 select * from t4;
+
+-- c
+create table t5(
+    name varchar(50),
+    price int,
+    price_tax numeric
+);
+
+create or replace function tax_add()
+returns trigger
+language plpgsql as
+$$
+    declare tax integer;
+    begin
+        new.price_tax = new.price + (new.price * 0.12);
+
+        return new;
+    end;
+$$;
+
+create trigger the_tax
+    before insert
+    on t5
+    for each row
+execute procedure tax_add();
+
+insert into t5 (name, price)
+values ('iPhone', 500),
+       ('Galaxy', 150);
+
+select * from t5;
+
+-- d
+-- e
+create table t6(
+    n1 integer,
+    n2 varchar(20),
+    n3 boolean
+);
+
+create or replace function call_func()
+returns trigger
+language plpgsql as
+$$
+    begin
+        new.n3 = check_password(new.n2);
+        perform (select (two_outputs2(new.n1)));
+
+        return new;
+    end;
+$$;
+
+create trigger two_func
+    after insert
+    on t6
+    for each row
+execute procedure call_func();
+
+insert into t6 (n1, n2)
+values (10, 'helloWorldabc');
+
+select * from t6;
+
+-- Task 3
+/*
+1.The procedure allows SELECT as well as
+DML(INSERT/UPDATE/DELETE) statement in it
+whereas Function allows only SELECT statement in it.
+2.Procedures cannot be utilized in a SELECT statement
+whereas Function can be embedded in a SELECT statement.
+*/
